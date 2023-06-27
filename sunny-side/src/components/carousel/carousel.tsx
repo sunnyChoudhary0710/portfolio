3ec6394248f4b9d0533ed7d6cd383ab1210e2b75
sunny-nagarro <sunny.choudhary@nagarro.com>
2023-06-27 15:36:55 +0530

    Scroll events added

--- sunny-side/src/components/carousel/carousel.tsx
+++ sunny-side/src/components/carousel/carousel.tsx
@@ -1,8 +1,62 @@
 "use client";
 import Image from "next/image";
 import { Data_Array } from "@/app/constants/constants";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 
 const Carousel = () => {
+	const [currentSlide, setCurrentSlide] = useState(0);
+	const ref = useRef() as MutableRefObject<HTMLDivElement>;
+	const [buttonDisabledController, setButtonDisabledController] = useState({
+		left: true,
+		right: false,
+	});
+
+	useEffect(() => {
+		const item = document.getElementById(`carousel-item-${currentSlide}`);
+		if (item && item.parentElement) {
+			item.parentElement.scroll({
+				left: item.offsetLeft,
+				top: 0,
+				behavior: "smooth",
+			});
+		}
+		console.log(ref.current.scrollLeft);
+		// if (ref && ref.current) {
+		// 	ref.current.style.transform = `translateX(${ref.current.offsetWidth})`
+		// }
+	}, [currentSlide]);
+
+	useEffect(() => {
+		setButtonDisabledController({
+			left: ref.current.scrollLeft === 0,
+			right:
+				ref.current.scrollWidth -
+					(ref.current.offsetWidth + ref.current.scrollLeft) ===
+				0,
+		});
+	}, []);
+
+	useEffect(() => {
+		ref.current.addEventListener("scroll", () => {
+			setButtonDisabledController({
+				left: ref.current.scrollLeft === 0,
+				right:
+					ref.current.scrollWidth -
+						(ref.current.offsetWidth + ref.current.scrollLeft) ===
+					0,
+			});
+		});
+		return ref.current.removeEventListener("scroll", () => {
+			setButtonDisabledController({
+				left: ref.current.scrollLeft === 0,
+				right:
+					ref.current.scrollWidth -
+						(ref.current.offsetWidth + ref.current.scrollLeft) ===
+					0,
+			});
+		});
+	}, []);
+
 	return (
 		<section className="w-full bg-port-yellow-ultra-light py-12 prose-xl">
 			<div className="flex flex-col gap-8">
@@ -10,10 +64,17 @@ const Carousel = () => {
 					Carousel{" "}
 					<span className="relative after:inline-text-underliner">Title</span>
 				</h2>
-				<div className="invisibility-scroll overflow-x-auto whitespace-nowrap snap-x snap-mandatory">
-					{Data_Array.map((item) => {
+				<div
+					ref={ref}
+					className="invisibility-scroll overflow-x-auto whitespace-nowrap snap-x snap-mandatory transition-all"
+				>
+					{Data_Array.map((item, i) => {
 						return (
-							<article className="w-3/4 lg:w-1/5 inline-block mx-5 lg:mx-8 snap-start scroll-ml-9 lg:scroll-mx-12">
+							<article
+								key={i}
+								id={`carousel-item-${i}`}
+								className="w-3/4 lg:w-1/5 inline-block mx-5 lg:mx-8 snap-start scroll-ml-9 lg:scroll-mx-12"
+							>
 								<Image
 									className="rounded-t-full m-0"
 									blurDataURL={item.blurDataUrl}
@@ -34,14 +95,33 @@ const Carousel = () => {
 					})}
 				</div>
 				<div className="flex flex-row gap-5 items-start px-9 lg:px-12">
-					<a className="px-7 py-4 rounded-full bg-port-yellow-light transition-all hover:bg-port-pink-base hover:-translate-x-2 hover:cursor-pointer">
+					<a
+						className={`px-7 py-4 rounded-full bg-port-yellow-light transition-all hover:bg-port-pink-base hover:-translate-x-2 hover:cursor-pointer ${
+							buttonDisabledController.left
+								? "hover:cursor-not-allowed opacity-30"
+								: ""
+						}`}
+						onClick={() => {
+							currentSlide !== 0 && setCurrentSlide(currentSlide - 1);
+						}}
+					>
 						<img
 							className="rotate-180 w-11 h-auto m-0"
 							src="arrow.svg"
 							alt="Go Left"
 						></img>
 					</a>
-					<a className="px-7 py-4 h-fit rounded-full bg-port-yellow-light transition-all hover:bg-port-pink-base hover:translate-x-2  hover:cursor-pointer">
+					<a
+						className={`px-7 py-4 rounded-full bg-port-yellow-light transition-all hover:bg-port-pink-base hover:-translate-x-2 hover:cursor-pointer ${
+							buttonDisabledController.right
+								? "hover:cursor-not-allowed opacity-30"
+								: ""
+						}`}
+						onClick={() => {
+							currentSlide !== Data_Array.length - 1 &&
+								setCurrentSlide(currentSlide + 1);
+						}}
+					>
 						<img
 							className="w-11 h-auto m-0"
 							src="arrow.svg"
